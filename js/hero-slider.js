ce41569471f06972589e0a8398d2b6a35deaa7dd
Полина <123145524+polshtva@users.noreply.github.com>
2024-02-22 09:43:57 +0500

    Add files via upload

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -1,18 +1,20 @@
-const itemImg = document.querySelectorAll(".hero__img-home");
-let currentImg = 0;
-let intervalId;
+document.addEventListener("DOMContentLoaded", function () {
+  const itemImg = document.querySelectorAll(".hero__img-home");
+  let currentImg = 0;
+  let intervalId;
 
-function imgSlider(item) {
-  item[currentImg].style.display = "none";
-  currentImg = (currentImg + 1) % item.length;
-  item[currentImg].style.display = "block";
-  item[currentImg].classList.add("hero__img-home");
-}
+  function imgSlider(item) {
+      item[currentImg].style.display = "none";
+      currentImg = (currentImg + 1) % item.length;
+      item[currentImg].style.display = "block";
+      item[currentImg].classList.add("hero__img-home");
+  }
 
-function startSlider(item) {
-  intervalId = setInterval(() => {
-    imgSlider(item);
-  }, 3000);
-}
+  function startSlider(item) {
+      intervalId = setInterval(() => {
+          imgSlider(item);
+      }, 3000);
+  }
 
-startSlider(itemImg);
+  startSlider(itemImg);
+});
